6829889b357b5a96171a5b717f0b277d05f1d7a3
FuryingFox <me@furyingfox.xyz>
2020-10-29 11:58:59 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,14 +4,12 @@ class settings(object):
     def __init__(self):
         self.title = "Schoolgame"
         self.borderless = False
-        self.show_fps = False
         self.show_x = False
 
 data = settings()
 
 window.title = data.title
 window.borderless = data.borderless
-window.fps_counter.enabled = data.show_fps
 window.exit_button.visible = data.show_x
 
 camera.fov = 90
